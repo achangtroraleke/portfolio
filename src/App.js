df52512055f5632722e531ae4e32f986da35649e
achangtroraleke <atachart.changtroraleke@gmail.com>
2023-07-06 23:34:06 -0400

    Fixed CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Header from './components/Header';
 import IntroductionSection from './components/Introduction';
 import AboutSection from './components/About';
 import ProjectSection from './components/Projects';
