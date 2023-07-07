4360dd3f4a380eca8ae329dfb9d849e860c65903
achangtroraleke <atachart.changtroraleke@gmail.com>
2023-07-06 23:40:14 -0400

    Fixed spelling mistakes

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const AboutSection =()=>{
                     <h1 className="header">About Me</h1>
                 </div>
                 <div className="wrapper">
-                <p>I am a James, a pasionate, Web Developer, specializing in React.js for frontend applications and Django for backend data storage. My expertise is creating tools that automate and solve various tasks and creating online enviroments where users can interact with one another.</p>
+                <p>I am James, a passionate, Web Developer, specializing in React.js for frontend designs and Django for backend data storage for most projects. My expertise is creating tools that automate and solve various tasks and creating online enviroments where users can interact with one another.</p>
             
                 </div>
            </div>
