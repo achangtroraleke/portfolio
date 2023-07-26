a6c84a878b7d1167471feeb6fbe1a2b4e7a3d460
achangtroraleke <atachart.changtroraleke@gmail.com>
2023-07-26 05:35:10 -0400

    update resume

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -49,7 +49,7 @@ const ProjectSection = ()=>{
 
     return(
         <section className="project-section">
-            <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill" fill="#9BCDD2" fill-opacity="1"></path></svg>
+            <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill" fill="#9BCDD2" fillOpacity="1"></path></svg>
             <div className="container flex-column center project-layout">
             <h1 className="main-text center-text">Projects</h1>
                 <div className="wrapper center">
@@ -76,7 +76,7 @@ const ProjectSection = ()=>{
                         >
                             {projects.map((project,index)=>{
                                 return(
-                                    <SwiperSlide>
+                                    <SwiperSlide key={index}>
                                     <div className="project-box flex-column center">
                                     <img src={require('../assets/images/'+project.image+'.png')} alt="project"></img>
                                     <h3 className="center-text project-title">{project.title}</h3>
@@ -98,7 +98,7 @@ const ProjectSection = ()=>{
                         </div>
                
                 </div>
-                <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill" fill="#FAF0E4" fill-opacity="1"></path></svg>
+                <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill" fill="#FAF0E4" fillOpacity="1"></path></svg>
             </div>
             
 
